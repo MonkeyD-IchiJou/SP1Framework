130905f902ea7997ab473ee811e2576ce16b54cb
TanJunYan <junyaaan@gmail.com>
2014-08-19 09:19:29 +0800

    ggg

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -93,18 +93,98 @@ void render()
     
     //render time taken to calculate this frame
     gotoXY(40, 0);
+
+    //render the game
+
+    //render test screen code (not efficient at all)
+    const WORD colors[] =   {
+        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
+        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6,
+    };
+
+    /* for (int i = 0; i < 12; ++i)
+    {
+    gotoXY(3*i,i+1);
+    colour(colors[i]);
+    std::cout << "WOW";
+    }*/
+    /*
+    // render time taken to calculate this frame
+    gotoXY(70, 0);
+>>>>>>> Stashed changes
     colour(0x1A);
     std::cout << 1.0 / deltaTime << "fps" << std::endl;
 
     gotoXY(0, 0);
     colour(0x59);
     std::cout << elapsedTime << "secs" << std::endl;
+<<<<<<< Updated upstream
 	colour(0x0C);
+<<<<<<< cc4bda5f82abbb0790f6ba5dddf5ebf05e6715dc
+=======
+<<<<<<< b3647aec69e1d9ae5ef24331a85837610e0fce0b
+<<<<<<< 0bd2a6a8efe758294cede3307fe50fdbe99aaa28
+=======
+=======
+=======
+    colour(0x0C);
+>>>>>>> af96d7de8fa614280f7ed60ecd1bbfcc6897b878
+    */
+>>>>>>> 322836128c2e9e032f86392a4f19c3e9746f554a
 
     /*// render character
     gotoXY(charLocation);
     colour(0x0C);
     std::cout << (char)1;*/
+<<<<<<< cc4bda5f82abbb0790f6ba5dddf5ebf05e6715dc
+=======
+<<<<<<< b3647aec69e1d9ae5ef24331a85837610e0fce0b
+	
+	colour(0x00D);
+    char tetris_map[22][23] = {
+        "**********************",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+		"*                    *",
+=======
+
+    char tetris_map[22][23] = {
+        "**********************",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+>>>>>>> af96d7de8fa614280f7ed60ecd1bbfcc6897b878
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "*                    *",
+        "**********************"
+<<<<<<< b3647aec69e1d9ae5ef24331a85837610e0fce0b
+>>>>>>> 6742002588d4dd56b365fefd1fb3d941971c5e3a
+=======
+>>>>>>> af96d7de8fa614280f7ed60ecd1bbfcc6897b878
+>>>>>>> 322836128c2e9e032f86392a4f19c3e9746f554a
 
     /*// render character
     gotoXY(charLocation);
@@ -121,6 +201,99 @@ void render()
 	welcome_screen();   //Welcome Screen of the game is here
     tetris_info();      //Info is here 
     tetris_screen();    //Gameplay Map is here
+<<<<<<< bea628f4462ab4601181be17794cf4911b7b7c7e
 	pause_screen();     //Pause Screen is here
+=======
+<<<<<<< cc4bda5f82abbb0790f6ba5dddf5ebf05e6715dc
 
+=======
+    
+
+<<<<<<< 1ccf3a0e5f3d1119a3fe5bd3c9f3f434b2fd88c4
+	_getch();
+=======
+<<<<<<< 0bd2a6a8efe758294cede3307fe50fdbe99aaa28
+	//_getch();
+>>>>>>> 8713d293563d28b08034e2ad6cf23375b5c66b01
+
+	//colour(0x0C);
+<<<<<<< ab4bafbca8a531a53f3ed3c66f3a0cb405b23de2
+=======
+
+    // render character
+<<<<<<< 1ccf3a0e5f3d1119a3fe5bd3c9f3f434b2fd88c4
+    
+=======
+    /*gotoXY(charLocation);
+    colour(0x0C);
+    std::cout << (char)1;*/
+<<<<<<< b3647aec69e1d9ae5ef24331a85837610e0fce0b
+=======
+=======
+    for(int i = 0; i < 22; i++)
+    {
+        gotoXY(0, 4 + i); 
+
+>>>>>>> af96d7de8fa614280f7ed60ecd1bbfcc6897b878
+        for(int j = 0; j < 23; j++)
+        {
+            std::cout << tetris_map[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+<<<<<<< b3647aec69e1d9ae5ef24331a85837610e0fce0b
+	colour(0x00E);
+	gotoXY(18,0);
+	std::cout << "ICHIproduction";
+
+	gotoXY(60,4);
+	std::cout << "Next in line Block"; 
+
+	gotoXY(8, 3);
+	std::cout << "TETRIS";
+
+	gotoXY(30,6);
+	std::cout << "Next in line Block";  
+
+	gotoXY(30,16);
+	std::cout << "Hold Block"; 
+
+
+	gotoXY(0, 26);
+	std::cout << "Your score is: ";
+	getch();
+
+	char block = '*';
+>>>>>>> 6742002588d4dd56b365fefd1fb3d941971c5e3a
+>>>>>>> 8713d293563d28b08034e2ad6cf23375b5c66b01
+>>>>>>> ea2d4a382fce1ccde880b03d8604f8205b95d1f9
+=======
+    gotoXY(18,0);
+    std::cout << "ICHIproduction";
+
+
+    gotoXY(60,4);
+    std::cout << "Next in line Block"; 
+
+    gotoXY(8, 3);
+    std::cout << "TETRIS";
+
+    gotoXY(30,6);
+    std::cout << "Next in line Block";  
+
+    gotoXY(30,16);
+    std::cout << "Hold Block"; 
+
+
+    gotoXY(0, 26);
+    std::cout << "Your score is: ";
+    getch();
+>>>>>>> 27e02543647d8ea5c055cd1ac49d6cef4390ac2d
+
+    char block = '*';
+>>>>>>> e874a60569dae7117757fa9ac54b20f817537940
+>>>>>>> 67e3a62cde6a0c223f72d4a0309a4a73e39abefc
+>>>>>>> af96d7de8fa614280f7ed60ecd1bbfcc6897b878
+>>>>>>> 322836128c2e9e032f86392a4f19c3e9746f554a
 }
