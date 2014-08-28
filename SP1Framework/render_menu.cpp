8c841f004b38fa2b30a2b5482e038c79e0b0b9d7
TanJunYan <junyaaan@gmail.com>
2014-08-28 18:01:26 +0800

    .

--- SP1Framework/render_menu.cpp
+++ SP1Framework/render_menu.cpp
@@ -4,13 +4,21 @@
 
 void renderMenu (COORD c)
 {
+    
+    
     gotoXY(c);
 	colour(White);
     cout << ">>";
 
+<<<<<<< 577c1c6ee86514be23a66501e64c3ab889843ffb
     gotoXY(32, 5);
     colour(Green);
 
+=======
+    gotoXY(30, 10);
+    gotoXY(32, 5);
+    colour(Green);
+>>>>>>> e72da49ac99736d9f7cb1877b60c4e7ba0748c99
     cout << "Start Game";
 
     gotoXY(32, 10);
@@ -29,4 +37,5 @@ void renderMenu (COORD c)
 
     gotoXY(58, 27);
     cout << "*Enter key to confirm";
+    colour(0x1A);
 }
